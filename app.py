f2b9d604d4e58952f789bb98ba106de21c6081af
Christian <kalonjic34@gmail.com>
2025-07-31 15:03:02 +0200

    Implement home page with initial todos

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
- 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def todo():
+    return render_template('home.html', todos = ['Get milk', 'Learn programming'])
